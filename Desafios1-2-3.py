6963a52cf406af926f8a3e5ddd4ce41a170c2fa3
kielkow <matheuskiel@fiorifer.com.br>
2019-02-22 12:06:39 -0300

    Desafios 1,2 e 3

---
+++ Desafios1-2-3.py
@@ -0,0 +1,13 @@
+nome = input('Qual é seu nome?')
+print('Ola', nome,'!' 'Bem Vindo!')
+
+dia = input('Digite o dia que voce nasceu:')
+mes = input('Digite o mes que voce nasceu:')
+ano = input('Digite o ano que voce nasceu:')
+
+print(nome ,'nasceu em' ,dia, 'de' ,mes, 'de' ,ano)
+
+x = input('Digite o primeiro numero:')
+y = input('Digite o segundo numero:')
+soma = int(x) + int(y)
+print('A soma dos numeros digitados é igual a',soma)
